2b985f67e83082529902507333ce7213a5f4e5cc
choadd <choadd@naver.com>
2024-07-01 13:48:56 +0000

    Modify Project name

---
+++ project/shop/src/includes/sample/shop_sample_base.h
@@ -0,0 +1,6 @@
+#ifndef __SHOP_SAMPLE_BASE_H__
+#define __SHOP_SAMPLE_BASE_H__
+
+void printHello();
+
+#endif /* __SHOP_SAMPLE_BASE_H__ */
---
+++ project/shop/src/sample/shop_sample_base.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void printHello(){
+    printf("Owner : Nice to meet you!\n");
+}
